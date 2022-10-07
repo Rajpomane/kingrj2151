f59486690d921f887a8dcac5a4661518ffa47697
Mr.Programmer <109902449+MrProgrammer72@users.noreply.github.com>
2022-10-07 17:05:38 +0530

    Update main.py

--- main.py
+++ main.py
@@ -11,13 +11,14 @@ API_ID = os.environ.get("API_ID", None)
 API_HASH = os.environ.get("API_HASH", None) 
 BOT_TOKEN = os.environ.get("BOT_TOKEN", None) 
 MONGO_URL = os.environ.get("MONGO_URL", None)
-
+BOT_NAME = os.environ.get("BOT_NAME", None)
 
 bot = Client(
     "AllegianceBot" ,
     api_id = API_ID,
     api_hash = API_HASH ,
-    bot_token = BOT_TOKEN
+    bot_token = BOT_TOKEN,
+    bot_name = BOT_NAME
 )
 
 
